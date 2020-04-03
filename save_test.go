00ea10224928c11af0aedbc56e24a2b5546f6305
feliixx <peteladrien@gmail.com>
2020-04-03 12:46:01 +1100

    improve test on saved playground metrics

--- save_test.go
+++ save_test.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"net/http"
+	"net/http/httptest"
 	"net/url"
+	"strings"
 	"testing"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func TestSave(t *testing.T) {
@@ -15,24 +21,28 @@ func TestSave(t *testing.T) {
 		params    url.Values
 		result    string
 		newRecord bool
+		mode      byte
 	}{
 		{
 			name:      "template config",
 			params:    templateParams,
 			result:    templateURL,
 			newRecord: true,
+			mode:      mgodatagenMode,
 		},
 		{
 			name:      "template config with new query",
 			params:    url.Values{"mode": {"mgodatagen"}, "config": {templateConfig}, "query": {"db.collection.find({\"k\": 10})"}},
 			result:    "p/DYlGRQeO0bX",
 			newRecord: true,
+			mode:      mgodatagenMode,
 		},
 		{
 			name:      "invalid config",
 			params:    url.Values{"mode": {"mgodatagen"}, "config": {`[{}]`}, "query": {templateQuery}},
 			result:    "p/EMmfQADkGcq",
 			newRecord: true,
+			mode:      mgodatagenMode,
 		},
 		{
 			name:      "save existing playground",
@@ -45,18 +55,21 @@ func TestSave(t *testing.T) {
 			params:    url.Values{"mode": {"bson"}, "config": {`[{}]`}, "query": {templateQuery}},
 			result:    "p/4cOeA7NGLru",
 			newRecord: true,
+			mode:      bsonSingleCollection,
 		},
 		{
 			name:      "bson mutliple db",
 			params:    url.Values{"mode": {"bson"}, "config": {`db={"c1":[{k:1}],"c2":[]}`}, "query": {templateQuery}},
 			result:    "p/AHWNKW4GK50",
 			newRecord: true,
+			mode:      bsonMultipleCollection,
 		},
 		{
 			name:      "bson unknown",
 			params:    url.Values{"mode": {"bson"}, "config": {`unknown`}, "query": {templateQuery}},
 			result:    "p/qXPeSYPpatw",
 			newRecord: true,
+			mode:      unknown,
 		},
 	}
 
@@ -77,20 +90,57 @@ func TestSave(t *testing.T) {
 		}
 	})
 
+	nbMgoDatagen, nbBsonSingle, nbBsonMultiple, nbUnknown := 0, 0, 0, 0
 	// save without run should not create any database
-	nbMongoDatabases := 0
-	nbBadgerRecords := 0
+	nbMongoDatabases, nbBadgerRecords := 0, 0
 	for _, tt := range saveTests {
 		if tt.newRecord {
 			nbBadgerRecords++
+			switch tt.mode {
+			case mgodatagenMode:
+				nbMgoDatagen++
+			case bsonSingleCollection:
+				nbBsonSingle++
+			case bsonMultipleCollection:
+				nbBsonMultiple++
+			case unknown:
+				nbUnknown++
+			}
 		}
 	}
 	testStorageContent(t, nbMongoDatabases, nbBadgerRecords)
 
+	testPlaygroundStats(t, nbMgoDatagen, nbBsonSingle, nbBsonMultiple, nbUnknown)
+}
+
+func testPlaygroundStats(t *testing.T, nbMgoDatagen, nbBsonSingle, nbBsonMultiple, nbUnknown int) {
+
+	// reset saved playground metrics
+	savedPlayground.Reset()
 	err := testServer.computeSavedPlaygroundStats()
 	if err != nil {
 		t.Errorf("fail to get stats from saved playgrounds")
 	}
+	resp := httptest.NewRecorder()
+	req, _ := http.NewRequest(http.MethodGet, metricsEndpoint, nil)
+	promhttp.Handler().ServeHTTP(resp, req)
+
+	want := fmt.Sprintf(`saved_playground_count{type="bson_multiple_collection"} %d
+saved_playground_count{type="bson_single_collection"} %d
+saved_playground_count{type="mgodatagen"} %d
+saved_playground_count{type="unknown"} %d`, nbBsonMultiple, nbBsonSingle, nbMgoDatagen, nbUnknown)
+
+	lines := make([]string, 0, 4)
+	scanner := bufio.NewScanner(resp.Body)
+	for scanner.Scan() {
+		if strings.HasPrefix(scanner.Text(), "saved_playground_count") {
+			lines = append(lines, scanner.Text())
+		}
+	}
+	if got := strings.Join(lines, "\n"); want != got {
+		t.Errorf("expected %s\n but got\n %s", want, got)
+	}
+
 }
 
 func TestErrorOnSavePlaygroundTooBig(t *testing.T) {
