e7e76372b11077e2a5fcbbfa3d9e10eb16181e2a
feliixx <peteladrien@gmail.com>
2021-03-26 17:42:21 +1100

    refactoring + cleanup
    mainly removing useless code in ace/mode/mongo

--- internal/web/mode-mongo.js
+++ internal/web/mode-mongo.js
@@ -1,287 +1,180 @@
-define("ace/mode/mongo_highlight_rules",["require","exports","module","ace/lib/oop","ace/mode/text_highlight_rules"], function(require, exports, module) {
-"use strict";
+define("ace/mode/mongo_highlight_rules", ["require", "exports", "module", "ace/lib/oop", "ace/mode/text_highlight_rules"], function (require, exports, module) {
+    "use strict";
 
-var oop = require("../lib/oop");
-var TextHighlightRules = require("./text_highlight_rules").TextHighlightRules;
+    var oop = require("../lib/oop")
+    var TextHighlightRules = require("./text_highlight_rules").TextHighlightRules
 
-var MongoHighlightRules = function() {
+    var MongoHighlightRules = function () {
 
-    var keywordMapper = function (value) {
-        switch (value) {
-            case "ObjectId":
-            case "ISODate":
-            case "Timestamp":
-            case "NumberInt":
-            case "NumberLong":
-            case "NumberDecimal":
-            case "BinData":
-            case "true":
-            case "false":
-            case "null":
-            case "undefined":
-                return "constant.language"
-            case "db":
-            case "find":
-            case "aggregate":
-            case "update":
-            case "explain":
-                return "storage.function"
-            default:
-                return "identifier"
-        }
-    }
-    this.$rules = {
-        "start" : [
-            {token: "comment", regex: "//.*"},
-            {token: "string", regex: '["](?:(?:\\\\.)|(?:[^"\\\\]))*?["]'},
-            {token: "constant.numeric", regex: "0[xX][0-9a-fA-F]+\\b"},
-            {token: "constant.numeric", regex: "[+-]?\\d+(?:(?:\\.\\d*)?(?:[eE][+-]?\\d+)?)?\\b"},
-            {token: "paren.lparen", regex: "[[({]"},
-            {token: "paren.rparen", regex: "[\\])}]"},
-            {token: keywordMapper, regex: "[a-zA-Z_$][a-zA-Z0-9_$]*\\b"}
-        ]
-    };
-};
-
-oop.inherits(MongoHighlightRules, TextHighlightRules);
-
-exports.MongoHighlightRules = MongoHighlightRules;
-});
-
-define("ace/mode/matching_brace_outdent",["require","exports","module","ace/range"], function(require, exports, module) {
-"use strict";
-
-var Range = require("../range").Range;
-
-var MatchingBraceOutdent = function() {};
-
-(function() {
-
-    this.checkOutdent = function(line, input) {
-        if (! /^\s+$/.test(line))
-            return false;
-
-        return /^\s*\}/.test(input);
-    };
-
-    this.autoOutdent = function(doc, row) {
-        var line = doc.getLine(row);
-        var match = line.match(/^(\s*\})/);
-
-        if (!match) return 0;
-
-        var column = match[1].length;
-        var openBracePos = doc.findMatchingBracket({row: row, column: column});
-
-        if (!openBracePos || openBracePos.row == row) return 0;
-
-        var indent = this.$getIndent(doc.getLine(openBracePos.row));
-        doc.replace(new Range(row, 0, row, column-1), indent);
-    };
-
-    this.$getIndent = function(line) {
-        return line.match(/^\s*/)[0];
-    };
-
-}).call(MatchingBraceOutdent.prototype);
-
-exports.MatchingBraceOutdent = MatchingBraceOutdent;
-});
-
-define("ace/mode/folding/cstyle",["require","exports","module","ace/lib/oop","ace/range","ace/mode/folding/fold_mode"], function(require, exports, module) {
-"use strict";
-
-var oop = require("../../lib/oop");
-var Range = require("../../range").Range;
-var BaseFoldMode = require("./fold_mode").FoldMode;
-
-var FoldMode = exports.FoldMode = function(commentRegex) {
-    if (commentRegex) {
-        this.foldingStartMarker = new RegExp(
-            this.foldingStartMarker.source.replace(/\|[^|]*?$/, "|" + commentRegex.start)
-        );
-        this.foldingStopMarker = new RegExp(
-            this.foldingStopMarker.source.replace(/\|[^|]*?$/, "|" + commentRegex.end)
-        );
-    }
-};
-oop.inherits(FoldMode, BaseFoldMode);
-
-(function() {
-    
-    this.foldingStartMarker = /([\{\[\(])[^\}\]\)]*$|^\s*(\/\*)/;
-    this.foldingStopMarker = /^[^\[\{\(]*([\}\]\)])|^[\s\*]*(\*\/)/;
-    this.singleLineBlockCommentRe= /^\s*(\/\*).*\*\/\s*$/;
-    this.tripleStarBlockCommentRe = /^\s*(\/\*\*\*).*\*\/\s*$/;
-    this.startRegionRe = /^\s*(\/\*|\/\/)#?region\b/;
-    this._getFoldWidgetBase = this.getFoldWidget;
-    this.getFoldWidget = function(session, foldStyle, row) {
-        var line = session.getLine(row);
-    
-        if (this.singleLineBlockCommentRe.test(line)) {
-            if (!this.startRegionRe.test(line) && !this.tripleStarBlockCommentRe.test(line))
-                return "";
-        }
-    
-        var fw = this._getFoldWidgetBase(session, foldStyle, row);
-    
-        if (!fw && this.startRegionRe.test(line))
-            return "start"; // lineCommentRegionStart
-    
-        return fw;
-    };
-
-    this.getFoldWidgetRange = function(session, foldStyle, row, forceMultiline) {
-        var line = session.getLine(row);
-        
-        if (this.startRegionRe.test(line))
-            return this.getCommentRegionBlock(session, line, row);
-        
-        var match = line.match(this.foldingStartMarker);
-        if (match) {
-            var i = match.index;
-
-            if (match[1])
-                return this.openingBracketBlock(session, match[1], row, i);
-                
-            var range = session.getCommentFoldRange(row, i + match[0].length, 1);
-            
-            if (range && !range.isMultiLine()) {
-                if (forceMultiline) {
-                    range = this.getSectionRange(session, row);
-                } else if (foldStyle != "all")
-                    range = null;
+        var keywordMapper = function (value) {
+            switch (value) {
+                case "ObjectId":
+                case "ISODate":
+                case "Timestamp":
+                case "NumberInt":
+                case "NumberLong":
+                case "NumberDecimal":
+                case "BinData":
+                case "true":
+                case "false":
+                case "null":
+                case "undefined":
+                    return "constant.language"
+                case "db":
+                case "find":
+                case "aggregate":
+                case "update":
+                case "explain":
+                    return "storage.function"
+                default:
+                    return "identifier"
             }
-            
-            return range;
+        }
+        this.$rules = {
+            "start": [
+                {token: "comment", regex: "//.*"},
+                {token: "string", regex: '["](?:(?:\\\\.)|(?:[^"\\\\]))*?["]'},
+                {token: "constant.numeric", regex: "0[xX][0-9a-fA-F]+\\b"},
+                {token: "constant.numeric", regex: "[+-]?\\d+(?:(?:\\.\\d*)?(?:[eE][+-]?\\d+)?)?\\b"},
+                {token: "paren.lparen", regex: "[[({]"},
+                {token: "paren.rparen", regex: "[\\])}]"},
+                {token: keywordMapper, regex: "[a-zA-Z_$][a-zA-Z0-9_$]*\\b"}
+            ]
+        }
+    }
+
+    oop.inherits(MongoHighlightRules, TextHighlightRules)
+    exports.MongoHighlightRules = MongoHighlightRules
+});
+
+define("ace/mode/matching_brace_outdent", ["require", "exports", "module", "ace/range"], function (require, exports, module) {
+    "use strict";
+
+    var Range = require("../range").Range
+
+    var MatchingBraceOutdent = function () {
+
+        this.checkOutdent = function (line, input) {
+            if (!/^\s+$/.test(line)) {
+                return false
+            }
+            return /^\s*\}/.test(input)
         }
 
-        if (foldStyle === "markbegin")
-            return;
+        this.autoOutdent = function (doc, row) {
+            var line = doc.getLine(row)
+            var match = line.match(/^(\s*\})/)
 
-        var match = line.match(this.foldingStopMarker);
-        if (match) {
-            var i = match.index + match[0].length;
+            if (!match) {
+                return 0
+            }
 
-            if (match[1])
-                return this.closingBracketBlock(session, match[1], row, i);
+            var column = match[1].length
+            var openBracePos = doc.findMatchingBracket({row: row, column: column})
 
-            return session.getCommentFoldRange(row, i, -1);
+            if (!openBracePos || openBracePos.row == row) {
+                return 0
+            }
+
+            var indent = this.$getIndent(doc.getLine(openBracePos.row))
+            doc.replace(new Range(row, 0, row, column - 1), indent)
         }
-    };
-    
-    this.getSectionRange = function(session, row) {
-        var line = session.getLine(row);
-        var startIndent = line.search(/\S/);
-        var startRow = row;
-        var startColumn = line.length;
-        row = row + 1;
-        var endRow = row;
-        var maxRow = session.getLength();
-        while (++row < maxRow) {
-            line = session.getLine(row);
-            var indent = line.search(/\S/);
-            if (indent === -1)
-                continue;
-            if  (startIndent > indent)
-                break;
-            var subRange = this.getFoldWidgetRange(session, "all", row);
-            
-            if (subRange) {
-                if (subRange.start.row <= startRow) {
-                    break;
-                } else if (subRange.isMultiLine()) {
-                    row = subRange.end.row;
-                } else if (startIndent == indent) {
-                    break;
+
+        this.$getIndent = function (line) {
+            return line.match(/^\s*/)[0];
+        }
+    }
+
+    exports.MatchingBraceOutdent = MatchingBraceOutdent
+});
+
+define("ace/mode/folding/cstyle", ["require", "exports", "module", "ace/lib/oop", "ace/mode/folding/fold_mode"], function (require, exports, module) {
+    "use strict";
+
+    var oop = require("../../lib/oop")
+    var BaseFoldMode = require("./fold_mode").FoldMode
+
+    var FoldMode = function () {
+
+        this.foldingStartMarker = /([\{\[\(])[^\}\]\)]*$/
+        this.foldingStopMarker = /^[^\[\{\(]*([\}\]\)])/
+        this._getFoldWidgetBase = this.getFoldWidget
+        this.getFoldWidget = function (session, foldStyle, row) {
+            return this._getFoldWidgetBase(session, foldStyle, row)
+        }
+
+        this.getFoldWidgetRange = function (session, foldStyle, row, forceMultiline) {
+            var line = session.getLine(row)
+
+            var match = line.match(this.foldingStartMarker)
+            if (match.length >= 2) {
+                return this.openingBracketBlock(session, match[1], row, match.index)
+            }
+
+            if (foldStyle === "markbegin") {
+                return
+            }
+
+            match = line.match(this.foldingStopMarker)
+            if (match.length >= 2) {
+                return this.closingBracketBlock(session, match[1], row, match.index + match[0].length)
+            }
+        }
+    }
+
+    oop.inherits(FoldMode, BaseFoldMode)
+    exports.FoldMode = FoldMode
+});
+
+define("ace/mode/mongo", ["require", "exports", "module", "ace/lib/oop", "ace/mode/text", "ace/mode/mongo_highlight_rules", "ace/mode/matching_brace_outdent", "ace/mode/behaviour/cstyle", "ace/mode/folding/cstyle"], function (require, exports, module) {
+    "use strict";
+
+    var oop = require("../lib/oop")
+    var TextMode = require("./text").Mode
+    var HighlightRules = require("./mongo_highlight_rules").MongoHighlightRules
+    var MatchingBraceOutdent = require("./matching_brace_outdent").MatchingBraceOutdent
+    var CstyleBehaviour = require("./behaviour/cstyle").CstyleBehaviour
+    var CStyleFoldMode = require("./folding/cstyle").FoldMode
+
+    var Mode = function () {
+
+        this.HighlightRules = HighlightRules
+        this.$outdent = new MatchingBraceOutdent()
+        this.$behaviour = new CstyleBehaviour()
+        this.foldingRules = new CStyleFoldMode()
+
+        this.getNextLineIndent = function (state, line, tab) {
+            var indent = this.$getIndent(line)
+
+            if (state == "start") {
+                var match = line.match(/^.*[\{\(\[]\s*$/)
+                if (match) {
+                    indent += tab
                 }
             }
-            endRow = row;
-        }
-        
-        return new Range(startRow, startColumn, endRow, session.getLine(endRow).length);
-    };
-    this.getCommentRegionBlock = function(session, line, row) {
-        var startColumn = line.search(/\s*$/);
-        var maxRow = session.getLength();
-        var startRow = row;
-        
-        var re = /^\s*(?:\/\*|\/\/|--)#?(end)?region\b/;
-        var depth = 1;
-        while (++row < maxRow) {
-            line = session.getLine(row);
-            var m = re.exec(line);
-            if (!m) continue;
-            if (m[1]) depth--;
-            else depth++;
-
-            if (!depth) break;
+            return indent
         }
 
-        var endRow = row;
-        if (endRow > startRow) {
-            return new Range(startRow, startColumn, endRow, line.length);
+        this.checkOutdent = function (state, line, input) {
+            return this.$outdent.checkOutdent(line, input)
         }
-    };
 
-}).call(FoldMode.prototype);
+        this.autoOutdent = function (state, doc, row) {
+            this.$outdent.autoOutdent(doc, row)
+        }
 
+        this.$id = "ace/mode/mongo"
+    }
+
+    oop.inherits(Mode, TextMode)
+    exports.Mode = Mode
 });
 
-define("ace/mode/mongo",["require","exports","module","ace/lib/oop","ace/mode/text","ace/mode/mongo_highlight_rules","ace/mode/matching_brace_outdent","ace/mode/behaviour/cstyle","ace/mode/folding/cstyle"], function(require, exports, module) {
-"use strict";
-
-var oop = require("../lib/oop");
-var TextMode = require("./text").Mode;
-var HighlightRules = require("./mongo_highlight_rules").MongoHighlightRules;
-var MatchingBraceOutdent = require("./matching_brace_outdent").MatchingBraceOutdent;
-var CstyleBehaviour = require("./behaviour/cstyle").CstyleBehaviour;
-var CStyleFoldMode = require("./folding/cstyle").FoldMode;
-
-var Mode = function() {
-    this.HighlightRules = HighlightRules;
-    this.$outdent = new MatchingBraceOutdent();
-    this.$behaviour = new CstyleBehaviour();
-    this.foldingRules = new CStyleFoldMode();
-};
-oop.inherits(Mode, TextMode);
-
-(function() {
-
-    this.lineCommentStart = "//";
-
-    this.getNextLineIndent = function(state, line, tab) {
-        var indent = this.$getIndent(line);
-
-        if (state == "start") {
-            var match = line.match(/^.*[\{\(\[]\s*$/);
-            if (match) {
-                indent += tab;
-            }
-        }
-
-        return indent;
-    };
-
-    this.checkOutdent = function(state, line, input) {
-        return this.$outdent.checkOutdent(line, input);
-    };
-
-    this.autoOutdent = function(state, doc, row) {
-        this.$outdent.autoOutdent(doc, row);
-    };
-
-    this.$id = "ace/mode/mongo";
-
-}).call(Mode.prototype);
-
-exports.Mode = Mode;
-});
-(function() {
-    window.require(["ace/mode/mongo"], function(m) {
+(function () {
+    window.require(["ace/mode/mongo"], function (m) {
         if (typeof module == "object" && typeof exports == "object" && module) {
-            module.exports = m;
+            module.exports = m
         }
-    });
+    })
 })();
             
