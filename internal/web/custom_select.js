cfc43aa8437933e1da2616ad7114af9747b149a9
feliixx <peteladrien@gmail.com>
2022-03-02 13:59:20 -1000

    js: add license header to all files

--- internal/web/custom_select.js
+++ internal/web/custom_select.js
@@ -1,5 +1,27 @@
+/* ***** BEGIN LICENSE BLOCK *****
+* A simple dropdown in vanilla js
+*
+* Copyright (C) 2015 Zoltan Toth
+* Copyright (C) 2017 Adrien Petel
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published
+* by the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
 /**
- * adapatation from https://github.com/zoltantothcom/vanilla-js-dropdown by Zoltan Toth
+ * A simple dropdown in vanilla js. Adaptation from https://github.com/zoltantothcom/vanilla-js-dropdown
+ * by Zoltan Toth
  *
  * @class
  * @param {(string)} config.selectId - HTML id of the select.
