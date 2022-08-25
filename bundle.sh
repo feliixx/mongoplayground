89531f98bb21d418f114891e26e2c41a19c63faa
feliixx <peteladrien@gmail.com>
2022-08-24 18:21:05 -1000

    asset version: use hash
    use md5 hash of file instead of manually incrementing
    version numbers

--- bundle.sh
+++ bundle.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+HTML_HASH=$(md5sum internal/web/static/docs.html | cut -d " " -f1)
+sed -i "s/docs-.*\.html/docs-$HTML_HASH.html/" internal/web/playground.js
+
 # order of js files is important 
 {
   cat internal/web/ace.js 
@@ -19,6 +22,11 @@
 
 
 esbuild --minify bundle.js > internal/web/static/playground-min.js
+JS_HASH=$(md5sum internal/web/static/playground-min.js | cut -d " " -f1)
+sed -i "s/playground-min-.*\.js/playground-min-$JS_HASH.js/" internal/web/playground.html
+
 esbuild --minify internal/web/playground.css > internal/web/static/playground-min.css
+CSS_HASH=$(md5sum internal/web/static/playground-min.css | cut -d " " -f1)
+sed -i "s/playground-min-.*\.css/playground-min-$CSS_HASH.css/" internal/web/playground.html
 
 rm -f bundle.js
